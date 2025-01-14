9db5a3e954aced643a705565dc19b7b647144ac0
FadliNur31 <fdlirmdhnn31@gmail.com>
2025-01-14 22:01:33 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <Button variant="outline">Click me</Button>
+      <Button variant="outline">Click  me</Button>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
