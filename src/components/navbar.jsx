36ac8cf3585922d0feba45e0d9ff80206a2c88cf
FadliNur31 <fdlirmdhnn31@gmail.com>
2025-01-15 14:58:39 +0700

    testing shadcn and make a navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Button } from "./ui/button";
+import { Logo } from "./ui/logo";
+export const Navbar = () => {
+ const a = () =>{
+    console.log("Pantek")
+ }
+  return (
+    <div className="flex justify-between w-full">
+        <Logo/>
+        <div className="flex justify-center">
+            <Button variant="destructive" className="font-bold" onClick={a}>Home</Button>  
+        </div>
+    </div>
+  );
+};
---
+++ src/components/ui/logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Logo = () => {
+  return (
+    <p className="bg-gradient-to-b from-red-500 to-red-950 bg-clip-text leading-tight text-3xl font-bold text-transparent tracking-tighter">
+        ValTracker
+      </p>
+  )
+}
